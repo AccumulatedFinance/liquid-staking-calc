585ddcb512e8dba9823e8afc49eea282746e412a
ilzheev <a@lovedesign.ru>
2023-04-08 22:51:18 +0200

    add stacme incentives share, add wacme lp incentives calc

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ const REWARDS_TOKEN_ACCOUNT = "acc://accumulated.acme/staking-rewards"
 const TREASURY_TOKEN_ACCOUNT = "acc://accumulated.acme/treasury"
 const LIQUID_STAKING_TOKEN_ACCOUNT = "acc://accumulated.acme/staking"
 const INCENTIVES_TOKEN_ACCOUNT = "acc://accumulated.acme/incentives"
+const WACME_LP_INCENTIVES_TOKEN_ACCOUNT = "acc://accumulated.acme/wacme-lp-incentives"
 
 type Output struct {
 	URL          string `json:"url"`
@@ -62,13 +63,17 @@ func (o *Output) String() string {
 
 func main() {
 
-	// set distribution
+	// set distribution for liquid staking rewards
 	// https://docs.accumulated.finance/accumulated-finance/fees
 	// share is in bps (1% = 100)
 	outputs := &Outputs{}
 	outputs.Items = append(outputs.Items, &Output{
 		URL:   TREASURY_TOKEN_ACCOUNT,
-		Share: 2000,
+		Share: 1200,
+	})
+	outputs.Items = append(outputs.Items, &Output{
+		URL:   INCENTIVES_TOKEN_ACCOUNT,
+		Share: 800,
 	})
 	outputs.Items = append(outputs.Items, &Output{
 		URL:   LIQUID_STAKING_TOKEN_ACCOUNT,
@@ -84,11 +89,32 @@ func main() {
 		log.Fatal("Expected total shares: ", 10000, ", received: ", totalShare)
 	}
 
-	// calculator
-	fmt.Println("Calculating liquid staking rewards...")
+	// set distribution for wacme lp incentives
+	// https://docs.accumulated.finance/accumulated-finance/fees
+	// share is in bps (1% = 100)
+	outputs_wacme := &Outputs{}
+	outputs_wacme.Items = append(outputs_wacme.Items, &Output{
+		URL:   TREASURY_TOKEN_ACCOUNT,
+		Share: 800,
+	})
+	outputs_wacme.Items = append(outputs_wacme.Items, &Output{
+		URL:   INCENTIVES_TOKEN_ACCOUNT,
+		Share: 9200,
+	})
+
+	// validate shares
+	var totalShare_wacme int
+	for _, item := range outputs_wacme.Items {
+		totalShare_wacme += item.Share
+	}
+	if totalShare_wacme != 10000 {
+		log.Fatal("Expected total shares: ", 10000, ", received: ", totalShare_wacme)
+	}
 
 	client := accumulate.NewAccumulateClient(API_URL, 5)
 
+	// liquid staking calculator
+	fmt.Println("Calculating liquid staking rewards...")
 	fmt.Println("Getting account balance:", REWARDS_TOKEN_ACCOUNT)
 
 	tokenAccount, err := client.QueryTokenAccount(&accumulate.Params{URL: REWARDS_TOKEN_ACCOUNT})
@@ -110,11 +136,43 @@ func main() {
 
 	fmt.Println("Generating CLI params...")
 
-	json, err := json.Marshal(outputs.Items)
+	jsonPayload, err := json.Marshal(outputs.Items)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("{ type: sendTokens, to: %v }", string(json))
+	fmt.Printf("{ type: sendTokens, to: %v }", string(jsonPayload))
+	fmt.Println("")
+
+	// wacme lp incentives calculator
+	fmt.Println("Calculating WACME LP incentives...")
+	fmt.Println("Getting account balance:", WACME_LP_INCENTIVES_TOKEN_ACCOUNT)
+
+	tokenAccount, err = client.QueryTokenAccount(&accumulate.Params{URL: WACME_LP_INCENTIVES_TOKEN_ACCOUNT})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	balance = &Balance{}
+	balance.FromString(tokenAccount.Data.Balance)
+
+	fmt.Println("Balance:", balance.Human(), "ACME")
+
+	for _, item := range outputs_wacme.Items {
+
+		item.FromBalance(balance)
+		fmt.Println(item)
+
+	}
+
+	fmt.Println("Generating CLI params...")
+
+	jsonPayload, err = json.Marshal(outputs_wacme.Items)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("{ type: sendTokens, to: %v }", string(jsonPayload))
+	fmt.Println("")
 
 }
